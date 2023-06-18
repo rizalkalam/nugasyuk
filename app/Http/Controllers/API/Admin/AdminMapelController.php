6760a5206bc90fdf454dcbed2c047de4353449d6
rizalkalam <rizalkalam78@gmail.com>
2023-06-19 00:34:11 +0700

    fix: validation add and edit data mapel and jadwal, add data detail mapel (role admin)

--- app/Http/Controllers/API/Admin/AdminMapelController.php
+++ app/Http/Controllers/API/Admin/AdminMapelController.php
@@ -54,6 +54,14 @@ class AdminMapelController extends Controller
             'asset_id' => 'required'
         ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors(),
+                'data' => [],
+            ]);
+        }
+
         $data = Mapel::create([
             'kode_id' => $request->kode_id,
             'kelas_id' => $request->kelas_id,
@@ -114,5 +122,53 @@ class AdminMapelController extends Controller
             'success' => true,
             'message' => 'Data jadwal berhasil di hapus',
         ]);
+    }
+
+    public function detail_mapel($id)
+    {
+        $data = Mapel::join('kodes', 'kodes.id', '=', 'mapels.kode_id')
+        ->join('assets', 'assets.id', '=', 'mapels.asset_id')
+        ->join('gurus', 'gurus.id', '=', 'kodes.guru_id')
+        ->join('kelas', 'kelas.id', '=', 'mapels.kelas_id')
+        ->join('tingkatans', 'tingkatans.id', '=', 'kelas.tingkatan_id')
+        ->join('jurusans', 'jurusans.id', '=', 'kelas.jurusan_id')
+        ->where('mapels.id', $id)
+        ->select([
+            'mapels.id',
+            'kodes.nama_mapel',
+            'gurus.nama_guru',
+            'tingkatans.tingkat_ke',
+            'jurusans.nama_jurusan',
+            'kelas.nama_kelas',
+            'assets.file_asset'
+        ])->first();
+
+        $kode_id = Mapel::join('kodes', 'kodes.id', '=', 'mapels.kode_id')
+        ->where('mapels.id', $id)->value('kodes.id');
+
+        $kelas_id = Mapel::join('kelas', 'kelas.id', '=', 'mapels.kelas_id')
+        ->where('mapels.id', $id)->value('kelas.id');
+
+        $asset_id =  Mapel::join('assets', 'assets.id', '=', 'mapels.asset_id')
+        ->where('mapels.id', $id)->value('assets.id');
+
+        $data = [
+            "id"=>$data->id,
+            "nama_mapel"=>$data->nama_mapel,
+            "nama_guru"=>$data->nama_guru,
+            "tingkat_ke"=>$data->tingkat_ke,
+            "nama_jurusan"=>$data->nama_jurusan,
+            "nama_kelas"=>$data->nama_kelas,
+            "file_asset"=>$data->file_asset,
+            "kode_id"=>$kode_id,
+            "kelas_id"=>$kelas_id,
+            "asset_id"=>$asset_id
+        ];
+
+        return response()->json([
+            "success" => true,
+            "message" => "Detail Mata Pelajaran",
+            "data" => $data,
+        ], 200);
     }
 }
