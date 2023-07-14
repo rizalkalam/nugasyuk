42681c2fa7ef2c7e822240c1737fdebc938757de
rizalkalam <rizalkalam78@gmail.com>
2023-07-14 15:46:13 +0700

    fix: pusher chat

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -11,9 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::drop('failed_jobs', function (Blueprint $table) {
-           
-        });
+        Schema::drop('failed_jobs');
     }
 
     /**
--- database/migrations/2023_07_14_060606_create_jobs_table.php
+++ database/migrations/2023_07_14_060606_create_jobs_table.php
@@ -11,9 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::drop('jobs', function (Blueprint $table) {
-          
-        });
+        Schema::drop('jobs');
     }
 
     /**
