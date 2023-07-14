a5d3f7e63a40b187c2c9db75c44623d48a02a5f2
rizalkalam <rizalkalam78@gmail.com>
2023-07-14 15:37:55 +0700

    fix: pusher chat

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -12,13 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::drop('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+           
         });
     }
 
