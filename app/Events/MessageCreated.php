a8cdbe36176f16d6236ccaae6a0889fcad9e454b
rizalkalam <rizalkalam78@gmail.com>
2023-07-14 13:07:29 +0700

    fix: pusher chat

--- app/Events/MessageCreated.php
+++ app/Events/MessageCreated.php
@@ -2,17 +2,19 @@
 
 namespace App\Events;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class MessageCreated implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, InteractsWithSockets, InteractsWithQueue, Queueable, SerializesModels;
 
     public $pesan;
 
