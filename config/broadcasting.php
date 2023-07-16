08186dfa6f1800e88d2bc605d880981097623c7e
rizalkalam <rizalkalam78@gmail.com>
2023-07-16 14:59:08 +0700

    fix: chat pusher -3

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+                'host' => 'https://www.nugasyuk.my.id',
+                'port' => 6001,
+                'scheme' => 'https'
             ],
         ],
        
