35275f82331374f846c3b9af177438f18ba2becb
rizalkalam <rizalkalam78@gmail.com>
2023-07-16 22:38:46 +0700

    fix: chat pusher -11

--- app/Events/MessageCreated.php
+++ app/Events/MessageCreated.php
@@ -12,7 +12,7 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class MessageCreated implements ShouldBroadcastNow
+class MessageCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
